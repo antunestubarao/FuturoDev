1616c80d5a3d8eb6e6b855fb59e270fc829a2d9e
Rodrigo Antunes <antunestubarao@gmail.com>
2023-12-17 16:22:35 -0300

    finalizado exercio M1S07EX01

--- Semana 08/app.js
+++ Semana 08/app.js
@@ -1,14 +1,1 @@
-const ptexto = document.getElementById('textosalva');
-const campotexto = document.getElementById('campotexto');
-const botaoSalva = document.getElementById('botaosalva');
-
-let textoatual= "texto inicial";
-
-function atualizatela() {
-    ptexto.innerHTML = textoatual;
-}
-
-function salvartexto() {
-    ptexto.innerHTML = textoatual;
-}
-botaoSalva.addEventListener('click', salvartexto);
+console.log("teste node");
